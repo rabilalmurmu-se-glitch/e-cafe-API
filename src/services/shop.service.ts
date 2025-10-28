1453c4ef78eeb6a983bcda34a5129d601598e610
unknown <rabilalmurmu.se@gmail.com>
2025-10-28 11:42:03 +0530

    remove unwanted files

--- src/services/shop.service.ts
+++
@@ -1,9 +0,0 @@
-import { BaseService } from "./base.service";
-
-class ShopService extends BaseService<"shop"> {
-  constructor() {
-    super("shop");
-  }
-}
-
-export const shopServices = new ShopService();
--- src/services/types.service.ts
+++
@@ -1,9 +0,0 @@
-import { BaseService } from "./base.service";
-
-class TypesService extends BaseService<"types"> {
-  constructor() {
-    super("types");
-  }
-}
-
-export const typesService = new TypesService();
